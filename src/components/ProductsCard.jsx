be8faa3ab45c68c8f1144445ebc0dd070ab114d4
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-22 19:18:18 +0100

    create card component

---
+++ src/components/ProductsCard.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export default function ProductsCard({ key, image, title, price }) {
+  return (
+    <div className="col">
+      <div className="card h-100 d-flex flex-column">
+        <Link
+          to={"/products/" + key}
+          className="text-decoration-none link-body-emphasis flex-grow-1"
+        >
+          <figure className="p-2 flex-grow-1 align-self-center img-container">
+            <img src={image} className="card-img-top product-img" alt={title} />
+          </figure>
+          <div className="card-body d-flex flex-column justify-content-center align-items-center">
+            <h5 className="card-title">{title}</h5>
+          </div>
+        </Link>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="fs-6">&euro;{price}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useBudgetMode } from "../contexts/BudgetContext";
+import ProductsCard from "../components/ProductsCard";
 
 export default function Products() {
   const { budgetMode } = useBudgetMode(false);
@@ -26,30 +27,12 @@ export default function Products() {
     <div className="row row-cols-5 g-0">
       {visibileProducts.map((product) => {
         return (
-          <div key={product.id} className="col">
-            <div className="card h-100 d-flex flex-column">
-              <Link
-                to={"/products/" + product.id}
-                className="text-decoration-none link-body-emphasis flex-grow-1"
-              >
-                <figure className="p-2 flex-grow-1 align-self-center img-container">
-                  <img
-                    src={product.image}
-                    className="card-img-top product-img"
-                    alt={product.title}
-                  />
-                </figure>
-                <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                  <h5 className="card-title">{product.title}</h5>
-                </div>
-              </Link>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <span className="fs-6">&euro;{product.price}</span>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <ProductsCard
+            key={product.id}
+            image={product.image}
+            title={product.title}
+            price={product.price}
+          />
         );
       })}
     </div>
