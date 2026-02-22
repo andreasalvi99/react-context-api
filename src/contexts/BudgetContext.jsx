971f27cb8052e5198e853bade3d82720a8f3e7dc
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-22 17:09:33 +0100

    write context budgetmode

---
+++ src/contexts/BudgetContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const BudgetContext = createContext();
+
+function BudgetModeProvider({ children }) {
+  const [budgetMode, setBudgetMode] = useState(false);
+  const contextValue = { budgetMode, setBudgetMode };
+
+  return (
+    <BudgetContext.Provider value={contextValue}>
+      {children}
+    </BudgetContext.Provider>
+  );
+}
+
+function useBudgetMode() {
+  return (useContext = BudgetContext);
+}
+
+export { BudgetContext, BudgetModeProvider };
